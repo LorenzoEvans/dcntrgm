31219db1d0b66d0551e9f7416da4af9531003ee8
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-04-01 14:17:30 -0700

    Got the javascript working

--- src/components/base-app-components/BaseApp.js
+++ src/components/base-app-components/BaseApp.js
@@ -6,6 +6,16 @@ export default class BaseApp extends Component {
     const { handleSignOut } = this.props;
     return (
       <>
+      <ul className="collapsible">
+        <li>
+          <div className="collapsible-header">First</div>
+          <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+        </li>
+        <li>stuff</li>
+        <li>stuff</li>
+        <li>stuff</li>
+        <li>stuff</li>
+        </ul>
         <button onClick={handleSignOut}>
         Signout
         </button>
