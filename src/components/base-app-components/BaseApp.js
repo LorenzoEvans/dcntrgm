9125af16d0c13188e7b747df81383d31ae5e6d26
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-04-01 17:32:10 -0700

    Alright going to call UI a wrap for today

--- src/components/base-app-components/BaseApp.js
+++ src/components/base-app-components/BaseApp.js
@@ -1,31 +1,30 @@
 import React, { Component } from 'react';
 import "../../styles/_globals.scss";
+import "./BaseApp.scss";
 export default class BaseApp extends Component {
 
   render() {
     const { handleSignOut } = this.props;
     return (
       <>
-      <nav className="nav-extended">
-        <div className="nav-wrapper flex-row-even">
-          <a href="#">Phototopics</a>
-          <li>hi</li>
-          <li>yo</li>
+      <nav className="nav-extended flex-row-even">
+        <div className="nav-wrapper flex-row-even sn-shadow-micro w-30-pc">
+          DCNTRGM
         </div>
-        <div className="nav-content">
-          <ul className="tabs tabs-transparent">
-          <li className="tab"><a href="#">Tab 1</a></li>
+        <div className="full-width full-height">
+          <ul className="tabs tabs-transparent flex-row-even">
+          <li className="tab nav-bar-text self-center"><a href="#">The Commons</a></li>
             {/* React router links will be needed 
               link to the url in the href here
               then in App, make routers for these links
               may need to do subroutes
             */}
-            <li className="tab"><a href="#">Tab 2</a></li>
-            <li className="tab">Tab 3</li>
+            <li className="tab nav-bar-text"><a href="#">Phototopics</a></li>
+            <li className="tab nav-bar-text"><a href="#">Profile</a></li>
           </ul>
         </div>
       </nav>
-      <ul className="collapsible">
+      {/* <ul className="collapsible">
         <li>
           <div className="collapsible-header">First</div>
           <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
@@ -37,7 +36,7 @@ export default class BaseApp extends Component {
         </ul>
         <button onClick={handleSignOut}>
         Signout
-        </button>
+        </button> */}
       </>
     )
   }
