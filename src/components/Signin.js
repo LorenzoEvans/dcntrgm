cc3fb23ed080ef39649b2764163a0d2a3175ff89
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-04-08 15:32:03 -0700

    Should have made a left at the jQuery

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import '../styles/sign-in.scss';
-
+import Carousel from 'react-bootstrap/Carousel'
 export default class Signin extends Component {
 
   render() {
@@ -34,14 +34,34 @@ export default class Signin extends Component {
           </ul>
         </div>
       </nav>
-      <div className="center">
+      {/* <div className="center">
         <h1 className="display-4">Welcome To DCNTRGM</h1>
         <p className="lead">
           A space for phototopical, thoughtful, community based discussions.
         </p>
-      </div>
+      </div> */}
         <hr className="my-4"/>
-
+      <Carousel className="text-on-prim border-test bg-secondary">
+        <Carousel.Item className="border-test nav-bar-text bg-primary">
+          hgfjfjh
+          <Carousel.Caption className="border-test nav-bar-text">
+            <h3 className="nav-bar-text">First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+         </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+        <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+         </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" alt="A group of people talking." src="/assets/conversation-1.jpeg"/>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" alt="A group of people talking." src="/assets/conversation-1.jpeg"/>
+        </Carousel.Item>
+      </Carousel>
       </div>
     );
   }
