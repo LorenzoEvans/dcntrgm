823ab78687d171e028b95494fb2f93f6e1f8ee49
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-04-16 18:11:01 -0700

    Made some changes earlier, pushing those up

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -4,6 +4,8 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+
+const conversation_1 = 'https://images.unsplash.com/photo-1554896485-c6d2cc4111a8?ixlib=rb-1.2.1&auto=format&fit=crop&w=1649&q=80';
 export default class Signin extends Component {
 
   render() {
@@ -11,9 +13,9 @@ export default class Signin extends Component {
 
     return (
       <div className="primary-color full-vh">
-      <nav >
+      <nav>
         <div className="nav-wrapper">
-          <ul className="full-width flexrow-between-align-center full-height">
+          <ul className="full-width border-test flexrow-between-align-center full-height">
             <li className="site-name site-name-shadow lttr-spacing">
               DCNTRGM
             </li>
@@ -37,14 +39,14 @@ export default class Signin extends Component {
           </ul>
         </div>
       </nav>
-      <div className="center">
-        <h1 className="display-4">Welcome To DCNTRGM</h1>
-        <p className="lead">
+      <hr className="my-4"/>
+      <div className="container full-width border-test">
+        <div className="bg-image"></div>
+        <h1 className="display-4 container-text">Welcome To DCNTRGM</h1>
+        <p className="lead container-text">
           A space for phototopical, thoughtful, community based discussions.
         </p>
       </div>
-        <hr className="my-4"/>
-
       </div>
     );
   }
